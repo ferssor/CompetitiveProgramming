4067bf197356118c720d89e5ed87eadf45a91686
Fernando Nogueira <fernando.cossta@gmail.com>
2025-01-28 15:35:23 -0300

    Add CP_BOOK study files

---
+++ cp_book/modulo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  int a, b, m;
+  cin >> a >> b >> m;
+  cout << (a + b) % m << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  solve();
+}
---
+++ cp_book/short_names.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll; // Using typedef
+#define PB push_back; // Using define
+#define REP(i,a,b) for (int i = a; i <= b; i++) 
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  ll a = 123456789;
+  ll b = 234324852;
+  cout << a * b << endl;
+}
